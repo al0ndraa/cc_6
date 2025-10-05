a97a6da4e84ff9a5b03bcae01d44fc615ead5293
Alondra Miranda Pagan <alondramirandapagan@usf.edu>
2025-10-05 14:40:22 -0400

    first commit

---
+++ cc_6.js
@@ -0,0 +1,9 @@
+//Step2
+class employee {
+    constructor (name, department)
+    {this.name = name; this.department = department;}
+ describe()
+    {return `${this.name} works in the ${this.departmen} department.`;}
+}
+
+//Step3
